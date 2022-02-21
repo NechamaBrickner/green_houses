d6ded6e87cc86319cba3cb861393eab4beeb5063
NechamaBrickner <anyg1@yahoo.com>
2022-02-21 17:41:21 +0200

    changed the name of the crop loop added the code to run the random forest and a loop that classifies the rasters

--- run.R
+++ run.R
@@ -13,7 +13,7 @@ print(paste(t0, "-- Begin process"))
 # classes <- LoadTrainingData()
 
 # Work on each dataset  and study area separately
-segmentations <- lapply(study_areas$name, function(sa){
+crop_rasters <- lapply(study_areas$name, function(sa){
   lapply(tif_dirs_full, function(d) {
     # Get list of TIF files in each dir
     # Read into stack, and crop
@@ -24,7 +24,7 @@ segmentations <- lapply(study_areas$name, function(sa){
       # The directory name will be used to name the new, cropped tif file
       # 
       print(paste("In:", sa, "directory:", d))
-      print(paste0(sa,"_", tif_dirs_full, ".tif")) # can be name of raster?
+      #print(paste0(sa,"_", tif_dirs_full, ".tif")) # can be name of raster?
       study_area <- study_areas[study_areas$name == sa,]
       cropped <- CropDatasets(tif_list, study_area)
       crop_all_layers <- AddImageTexture(cropped)
@@ -55,3 +55,50 @@ segmentations <- lapply(study_areas$name, function(sa){
 t2 = Sys.time()
 elapsed = round(difftime(t2, t0, units = "mins"),2)
 print(paste(t2, "-- End process in", elapsed, "minutes"))
+
+
+### random forest 
+# create raster for classification
+raster1 = load_rast_4_RF(rast_4_RF, rast_shp )
+# add texture and index bands
+raster1 = addallbands(raster = raster1)
+# create training data
+TD = create_td(training_data_RF, allbands = raster1)
+# delete columns not used in the random forest 
+# will change depending on what model we use
+## normally has blue but for now has geen
+TD1 = TD %>%
+  select(-aerosol, -red, -blue, -SWIR2, -dissimilarity, - SAVI)
+# run the random forest model
+mod = Prepare_RF_Model(training_data = TD1)
+
+# get names of cropped raster's
+tif_cropped = list.files(cropped_dir)
+tif_cropped = tif_cropped[grep(pattern =".tif$", x=tif_cropped)]
+
+# to add to plots
+colors = c("gray", "yellow", "cyan", "dark green", "black", "blue")
+x = levels(TD$ground_type)
+
+# loop through the cropped raster's and classify them 
+
+classify_raster = lapply(tif_cropped, function(r){
+  print(paste0("cropped\\", r))
+  r_1 = rast(paste0("cropped\\", r)) # load raster
+  #plot(r_1$green, main = paste(r, "green band"))
+  classify_r = ApplyRFModel(all_rast_4_RF = r_1, fit = mod) # classify the raster
+  r_split <- strsplit(x=basename(r), split = ".", fixed = TRUE)
+  r_split <- unlist(r_split)[1]
+  rastname = paste( r_split, "classified", sep="_")
+  rastpath <- file.path(classified_dir, paste0(rastname, ".tif"))
+  plot(classify_r, col = colors, type = "classes", levels = x, main = paste(r, "classified"))
+  writeRaster(classify_r, filename = rastpath, overwrite = TRUE ) # save the raster
+  return(classify_r)
+})
+
+
+
+
+
+
+
