c717c860a5f0f90ed72d73efbf2da194c183252b
micha-silver <tsvibar@gmail.com>
2022-05-27 14:37:56 +0300

    Minor edits

--- run.R
+++ run.R
@@ -28,13 +28,13 @@ crop_rasters <- lapply(study_areas$name, function(sa){
     if (length(tif_list) > 0) {
       # pass both list of tif files, and containing directory to the cropping function
       # The directory name will be used to name the new, cropped tif file
-      # 
+      #
       print(paste("In:", sa, "directory:", d))
       #print(paste0(sa,"_", tif_dirs_full, ".tif")) # can be name of raster?
       study_area <- study_areas[study_areas$name == sa,]
       cropped <- CropDatasets(tif_list, study_area)
       crop_all_layers <- AddImageTexture(cropped)
-      
+
       #save the cropped images
       d_split <- strsplit(x=basename(d), split = "_", fixed = TRUE)
       datestr <- unlist(d_split)[4]
@@ -82,7 +82,7 @@ tif_cropped = list.files(cropped_dir, pattern = "tif$",
 #'---------------------------------
 #' Run classification
 #'---------------------------------
-# loop through the cropped raster's and classify them 
+# loop through the cropped raster's and classify them
 classified_rasters = lapply(tif_cropped, function(t){
   # The tif_cropped list already has full path to each file
   r = rast(t)
@@ -95,13 +95,11 @@ classified_rasters = lapply(tif_cropped, function(t){
   rastpath <- file.path(classified_dir, paste0(rastname, ".tif"))
   writeRaster(x = rast_classify, filename = rastpath,
               overwrite = TRUE)
-  
+
   return(rast_classify)
 })
 
 PlotClassified(tif_cropped, classified_rasters)
-
-
 #'---------------------------------
 #' Land surface temperature
 #'---------------------------------
@@ -112,19 +110,19 @@ LST_crop <- lapply(study_areas$name, function(sa){
     tif_list = list.files(d, pattern = "TIF$",
                           full.names = TRUE, recursive = TRUE)
     if (length(tif_list) > 0) {
-      
+
       print(paste("In:", sa, "directory:", d))
       study_area <- study_areas[study_areas$name == sa,]
       LST_b <- LST_band(tif_list, study_area)
       #print(range(LST_b))
-      
+
       d_split <- strsplit(x=basename(d), split = "_", fixed = TRUE)
       datestr <- unlist(d_split)[4]
       rastname = paste("LST", sa, datestr, sep="_")
       rastpath <- file.path(LST_dir, paste0(rastname, ".tif"))
       terra::writeRaster(x= LST_b, filename = rastpath, overwrite = TRUE)
       plot(LST_b, main = paste(rastname, "C", sep = " "))
-      
+
       return(LST_b)
     }
   })
@@ -137,6 +135,3 @@ LST_crop <- lapply(study_areas$name, function(sa){
 t2 = Sys.time()
 elapsed = round(difftime(t2, t0, units = "mins"),2)
 print(paste(t2, "-- End process in", elapsed, "minutes"))
-
-
-
