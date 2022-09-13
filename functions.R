6da551c947da465be727416df2300635ccd4bdf3
NechamaBrickner <anyg1@yahoo.com>
2022-09-13 15:19:27 +0300

    added a color palette

--- functions.R
+++ functions.R
@@ -285,3 +285,41 @@ change_sum = function(rast) {
   return(cd_sum)
 }
 
+
+# creating a discrete color palette
+color_pal_list = list(
+  light24 = c("#FD3216", "#00FE35", "#6A76FC", "#479B55", "#EEA6FB", "#0DF9FF", 
+              "#F6F926", "#FF9616", "#FED4C4", "#86CE00", "#DC587D", "#D626FF", 
+              "#6E899C", "#00B5F7", "#B68E00", "#C9FBE5", "#FF0092", "#22FFA7", 
+              "#E3EE9E", "#FE00CE", "#BC7196", "#7E7DCD", "#FC6955", "#E48F72"),                              
+  
+  dark24 = c("#2E91E5", "#E15F99", "#1CA71C", "#FB0D0D", "#DA16FF", "#222A2A",
+             "#B68100", "#750D86", "#EB663B", "#511CFB", "#00A08B", "#FB00D1",
+             "#FC0080", "#B2828D", "#6C7C32", "#778AAE", "#862A16", "#A777F1", 
+             "#620042", "#1616A7", "#DA60CA", "#6C4516", "#0D2A63", "#AF0038"), 
+  
+  alphabet = c("#AA0DFE", "#3283FE", "#85660D", "#782AB6", "#565656", "#1C8356",
+               "#16FF32", "#F7E1A0", "#E2E2E2", "#1CBE4F", "#C4451C", "#DEA0FD",
+               "#FE00FA", "#325A9B", "#FEAF16", "#F8A19F", "#90AD1C", "#F6222E", 
+               "#1CFFCE", "#2ED9FF", "#B10DA1", "#C075A6", "#FC1CBF", "#B00068", 
+               "#FBE426", "#FA0087")
+)
+
+color_palettes = function(name, n, all_palettes = color_pal_list, type = c("discrete", "continuous")) {
+  palette = all_palettes[[name]]
+  if (missing(n)) {
+    n = length(palette)
+  }
+  type = match.arg(type)
+  out = switch(type,
+               continuous = grDevices::colorRampPalette(palette)(n),
+               discrete = palette[1:n]
+  )
+  structure(out, name = name, class = "palette")
+}
+
+
+scale_fill_color_pal = function(name) {
+  ggplot2::scale_fill_manual(values = color_palettes(name,
+                                                     type = "discrete"))
+}
